f0cfdbd635a5b59537fc46f0e70da6235ccc8794
Hal Seki <hal@plants-web.jp>
2022-02-12 21:00:14 +0900

    add avatar image function

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react';
 import { supabase } from '../utils/supabaseClient';
 import { Session } from '@supabase/supabase-js';
-import { Box, Stack, FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, Button } from '@chakra-ui/react';
+import { Box, Stack, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
 import { PrimaryButton } from './atoms/button/PrimaryButton';
+import Avatar from './Avatar';
 
 export default function Account({ session }: { session: Session }) {
   const [loading, setLoading] = useState<boolean>(true);
   const [username, setUsername] = useState<string | null>(null);
   const [website, setWebsite] = useState<string | null>(null);
   const [avatar_url, setAvatarUrl] = useState<string | null>(null);
-
   useEffect(() => {
     getProfile();
   }, [session]);
@@ -85,23 +85,29 @@ export default function Account({ session }: { session: Session }) {
   return (
     <Box shadow='md'>
       <Stack spacing={4} py={4} px={10}>
+        <Avatar
+          url={avatar_url}
+          size={150}
+          onUpload={(url) => {
+            setAvatarUrl(url);
+            updateProfile({ username, website, avatar_url: url });
+          }}
+        />
         <FormControl>
-          <FormLabel htmlFor='email'>Email</FormLabel>
+          <FormLabel htmlFor='email'>Email:</FormLabel>
           <Input id='email' type='text' value={session?.user?.email} disabled />
         </FormControl>
         <FormControl>
-          <FormLabel htmlFor='username'>Name</FormLabel>
+          <FormLabel htmlFor='username'>Name:</FormLabel>
           <Input id='username' type='text' value={username || ''} onChange={(e) => setUsername(e.target.value)} />
         </FormControl>
         <FormControl>
-          <FormLabel htmlFor='website'>Website</FormLabel>
+          <FormLabel htmlFor='website'>Website:</FormLabel>
           <Input id='website' type='website' value={website || ''} onChange={(e) => setWebsite(e.target.value)} />
         </FormControl>
-
         <PrimaryButton onClick={() => updateProfile({ username, website, avatar_url })} disabled={loading}>
           {loading ? 'Loading ...' : 'Update'}
         </PrimaryButton>
-
         <Button className='button block' onClick={() => supabase.auth.signOut()}>
           Sign Out
         </Button>
