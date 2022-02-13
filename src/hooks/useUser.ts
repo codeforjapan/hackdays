c7942c08df1510682bcd751a874e807023e890ce
Hal Seki <hal@plants-web.jp>
2022-02-13 14:34:28 +0900

    add GitHub auth

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,34 @@
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabaseClient";
+
+export default function userUser() {
+  const [session, setSession] = useState<Session>();
+
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (session != null)
+          setSession(session);
+      }
+    );
+
+    return () => {
+      authListener?.unsubscribe();
+    };
+  }, []);
+
+  function signInWithGithub() {
+    supabase.auth.signIn({ provider: "github" });
+  }
+
+  function signOut() {
+    supabase.auth.signOut();
+  }
+
+  return {
+    session,
+    signInWithGithub,
+    signOut,
+  };
+}
