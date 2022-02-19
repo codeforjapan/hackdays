0f82feac06c3134620a87f59bb400ad47655ff49
Hal Seki <hal@plants-web.jp>
2022-02-20 08:59:47 +0900

    change import

--- src/services/projects.service.ts
+++ src/services/projects.service.ts
@@ -1,5 +1,5 @@
 import { PostgrestResponse } from '@supabase/supabase-js';
-import { definitions } from '../../types/supabase';
+import { definitions } from '../types/supabase';
 import { supabase } from '../utils/supabaseClient';
 
 export const ProjectService = {
