edeb1fecb852b1bbc6c2658bf584338e6621b1df
Hal Seki <hal@plants-web.jp>
2022-02-20 10:36:09 +0900

    bad commit

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -2,7 +2,7 @@ import { Button } from '@chakra-ui/react';
 import { VFC, memo, ReactNode } from 'react';
 
 type Props = {
-  children: ReactNodeb;
+  children: ReactNodea;
   onClick: () => void;
   disabled?: boolean;
   loading?: boolean;
