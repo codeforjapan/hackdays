0fc3d89c347d43e62c180ffac0941d125a41586d
Hal Seki <hal@plants-web.jp>
2022-02-21 15:00:44 +0900

    Set useCallback for custom hooks

--- src/hooks/useAvatar.ts
+++ src/hooks/useAvatar.ts
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { supabase } from '../utils/supabaseClient';
 
 export default function useAvatar({ onUpload }: { onUpload: (url: string) => void }) {
   const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
   const [avatarUploading, setUploading] = useState(false);
-  async function downloadAvatar(path: string) {
+  const downloadAvatar = useCallback(async (path: string) => {
     try {
       const { data, error } = await supabase.storage.from('avatars').download(path);
       if (error) {
@@ -18,9 +18,9 @@ export default function useAvatar({ onUpload }: { onUpload: (url: string) => voi
     } catch (error) {
       if (error instanceof Error) console.log('Error downloading image: ', error.message);
     }
-  }
+  }, []);
 
-  async function uploadAvatar(event: React.ChangeEvent<HTMLInputElement>) {
+  const uploadAvatar = useCallback(async (event: React.ChangeEvent<HTMLInputElement>) => {
     try {
       setUploading(true);
 
@@ -45,7 +45,7 @@ export default function useAvatar({ onUpload }: { onUpload: (url: string) => voi
     } finally {
       setUploading(false);
     }
-  }
+  }, []);
   return {
     avatarUploading,
     avatarUrl,
