5cdbd056bce6096608b5471e8aaf46bcf584b080
Hal Seki <hal@plants-web.jp>
2022-03-06 22:12:32 +0900

    Cover 100% test for useAvatar

--- src/__tests__/hooks/useAvatar.test.tsx
+++ src/__tests__/hooks/useAvatar.test.tsx
@@ -3,6 +3,10 @@ import useAvatar from '../../hooks/useAvatar';
 import { StorageService } from '../../services/storage.service';
 
 describe('useAvatar test', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.fn().mockRestore();
+  });
   it('should upload a new image', async () => {
     expect.assertions(2);
     // load test file
@@ -26,6 +30,44 @@ describe('useAvatar test', () => {
     expect(mockedUpload).toBeCalled();
     expect(result.current.state.uploadSuccess).toBe(true);
   });
+  it('should raise error when uploading a new image failed', async () => {
+    expect.assertions(1);
+    // load test file
+    const file = new File(['hello'], '../../../../../public/images/cat.png', { type: 'image/png' });
+    const mockedUpload = jest.fn().mockRejectedValue(new Error('upload failed'));
+    // create onUpload event handler
+    const mockedOnUpload = jest.fn();
+    // custom hook object
+    const { result } = renderHook(() => useAvatar({ onUpload: mockedOnUpload }));
+    // set mocked function
+    StorageService.upload = mockedUpload;
+    // call uploadAvatar
+    await act(async () => {
+      const t = async () => {
+        await result.current.uploadAvatar(file);
+      };
+      expect(t).rejects.toThrow();
+    });
+  });
+  it('should raise error when uploading a new image returns error', async () => {
+    expect.assertions(1);
+    // load test file
+    const file = new File(['hello'], '../../../../../public/images/cat.png', { type: 'image/png' });
+    const mockedUpload = jest.fn().mockResolvedValue({ error: new Error('upload failed') });
+    // create onUpload event handler
+    const mockedOnUpload = jest.fn();
+    // custom hook object
+    const { result } = renderHook(() => useAvatar({ onUpload: mockedOnUpload }));
+    // set mocked function
+    StorageService.upload = mockedUpload;
+    // call uploadAvatar
+    await act(async () => {
+      const t = async () => {
+        await result.current.uploadAvatar(file);
+      };
+      expect(t).rejects.toThrow();
+    });
+  });
   it('should download specified image', async () => {
     expect.assertions(1);
     // mock downaload function
@@ -42,6 +84,54 @@ describe('useAvatar test', () => {
     });
     // download method should be called
     expect(mockedDownload).toBeCalled();
+  });
+  it('should throw error when download failed', async () => {
+    expect.assertions(1);
+    // mock downaload function
+    const mockedDownload = jest.fn().mockRejectedValue(new Error('donwload failed'));
+    // custom hook
+    const { result } = renderHook(() => useAvatar({ onUpload: jest.fn() }));
+    // set mocked function
+    StorageService.download = mockedDownload;
+    // call downloadAvatar
+    await act(async () => {
+      const t = async () => {
+        await result.current.downloadAvatar('test/avatar.png');
+      };
+      expect(t).rejects.toThrow();
+    });
+  });
+  it('should throw error when download returns error', async () => {
+    expect.assertions(1);
+    // mock downaload function
+    const mockedDownload = jest.fn().mockResolvedValue({ error: new Error('failed') });
+    // custom hook
+    const { result } = renderHook(() => useAvatar({ onUpload: jest.fn() }));
+    // set mocked function
+    StorageService.download = mockedDownload;
+    // call downloadAvatar
+    await act(async () => {
+      const t = async () => {
+        await result.current.downloadAvatar('test/avatar.png');
+      };
+      expect(t).rejects.toThrow();
+    });
+  });
+  it('should throw error when download returns null', async () => {
+    expect.assertions(1);
+    // mock downaload function
+    const mockedDownload = jest.fn().mockResolvedValue({ data: null });
+    // custom hook
+    const { result } = renderHook(() => useAvatar({ onUpload: jest.fn() }));
+    // set mocked function
+    StorageService.download = mockedDownload;
+    // call downloadAvatar
+    await act(async () => {
+      async function t() {
+        await result.current.downloadAvatar('test/avatar.png');
+      }
+      expect(t).rejects.toThrow();
+    });
   });
   it('should upload a new image and set a new URL of the image', async () => {
     expect.assertions(2);
